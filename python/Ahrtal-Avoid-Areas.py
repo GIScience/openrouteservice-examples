9d074fd14472bdaf7e0847bd0e99f0063db52fa8
christina <christina.ludwig@uni-heidelberg.de>
2023-06-26 17:46:58 +0200

    add text to Ahrtal Avoid Areas

--- python/Ahrtal-Avoid-Areas.py
+++ python/Ahrtal-Avoid-Areas.py
@@ -15,14 +15,17 @@
 
 # # Disaster aware routing with openrouteservice
 
-# ### Install and import necessary libraries
+# In times of disaster, such as the devastating flood event that occurred in the Ahr valley in Germany in July 2021, **routing becomes crucial for emergency responders, humanitarian organizations, and disaster management teams** to distribute help quickly and efficiently. Openrouteservice, an open-source routing engine built on OpenStreetMap data, offers a **powerful** and **flexible** solution for **navigation** and **logistics** **planning** in such scenarios.
 #
-# For this exercise we use the following R libraries:
+# This training aims to **familiarize participants with the capabilities of openrouteservice within Python** and demonstrate how it can be leveraged to address routing challenges during a disaster. Through a series of code examples we will explore the key features and functionalities of openrouteservice. You will learn how easy it is to interact with openrouteservice from Python and be able to integrate it directly into your own workflow.
+
+# # Context
 #
-# -   `dplyr` for the ease of manipulating data.frame classes Wickham H, François R, Henry L, Müller K, Vaughan D (2023). *dplyr: A Grammar of Data Manipulation*. R package version 1.1.2, <https://CRAN.R-project.org/package=dplyr>.
-# -   `sf` the dplyr equivalent to work with spatial data.frames. Pebesma, E., & Bivand, R. (2023). Spatial Data Science: With Applications in R (1st ed.). Chapman and Hall/CRC. <https://doi.org/10.1201/9780429459016>
-# -   `openrouteservice` is the package that provides a user firendly R interface to the openrouteservice API. The package is not published on CRAN, therefore we install it via the `devtools` package from github.
-# -   `mapview` is a easy to use library to create interactive maps. Appelhans T, Detsch F, Reudenbach C, Woellauer S (2022). *mapview: Interactive Viewing of Spatial Data in R*. R package version 2.11.0, <https://CRAN.R-project.org/package=mapview>.
+# During the Ahr valley flood event, many roads and bridges were damaged or rendered impassable, posing significant challenges for navigation and transportation. In such situations, openrouteservice can assist in finding alternative routes which avoid damaged roads to deliver essential supplies and services to affected areas.
+
+# # Setup
+
+# Load all necessary modules.
 
 from openrouteservice import client
 from openrouteservice.exceptions import ApiError
@@ -33,71 +36,130 @@ import matplotlib.pyplot as plt
 import folium
 from branca.colormap import linear
 from shapely.geometry import Point, mapping
-from ipyleaflet import Map, DrawControl, AwesomeIcon, Marker, GeoData, basemaps, LayersControl, Icon, GeoJSON
 
 # ### Set up openrouteservice
 
-# insert your ORS api key
-api_key = ''
-ors = client.Client(key=api_key)
+# Make sure that you have a [personal key for the public openrouteservice API](https://openrouteservice.org/dev/#/login). Insert **your API key** below.
 
-# ## Simple route with avoid area  
+api_key = '5b3ce3597851110001cf6248eef794d1244544f7826f417356aee9e4'
 
-# ### Normal route from disaster relief stagin area towards Bad Neuanahr-Ahrweiler 
+# **Create a ORS client** which will be used to send request to the API. 
 
-# +
-start_point = (6.943241, 50.334265)
-destination = (7.119166, 50.548979)
+# + jp-MarkdownHeadingCollapsed=true
+ors_client = client.Client(key=api_key)
+# -
 
-# Define parameters for request to openrouteservice
-request_params = {'coordinates': [start_point, destination],
+# # Simple routing and avoid areas
+#
+# Let's start to use the openrouteservice library by performing a **simple A-to-B routing task**. We start by defining an origin and destination and then call the API to create a route out of it. The coordinates provided in the code chunk point to the ***Nürburgring*, a motorsports complex in rural Rhineland-Palatinate, which served as staging area during the emergency response.** In this area, rescue helpers from all over Germany were stationed to sleep, eat and repair their equipment inbetween deployments. The destination coordinates point to ***Bad Neuenahr-Ahrweiler***, the largest community affected by the flood in Rhineland-Palatinate.
+
+# ## Create a route from Nürburgring to Bad Neuanahr-Ahrweiler
+
+# Defines the coordinates of the staging area, Nürburgring
+
+staging_area = (6.943241, 50.334265)
+staging_area_df = gpd.GeoDataFrame({'geometry': [Point(staging_area)], 'name': ['Bad Neuenahr']}, crs="epsg:4326")
+
+# Defines the coordinates of Bad Neuenahr-Ahrweiler
+
+badneuenahr_coordinates = [7.119166, 50.548979]
+badneuenahr_df = gpd.GeoDataFrame({'geometry': [Point(badneuenahr_coordinates)], 'name': ['Bad Neuenahr']}, crs="epsg:4326")
+
+# Let's define the request parameters 
+
+request_params = {'coordinates': [staging_area, bad_neuenahr_ahrweiler],
                  'format_out': 'geojson',
                  'profile': 'driving-hgv',
                  'preference': 'recommended',
                  'instructions': False}
-# -
 
 # Send request to ORS
 
-route_directions = ors.directions(**request_params)
+route_directions = ors_client.directions(**request_params)
 
 # Convert ORS response to GeoDataFrame
 
-route = gpd.GeoDataFrame().from_features(route_directions).set_crs(epsg=4326)
+route = gpd.GeoDataFrame.from_features(route_directions).set_crs(epsg=4326)
 
-route.explore()
+# Print it on an interactive map using `GeoPandas` great `explore()` method! 
 
-# #### Send request again with avoided polygon
+m = route.explore(tiles='cartodbpositron')
+staging_area_df.explore(m=m, color='red', marker_kwds={'radius': 15})
+badneuenahr_df.explore(m=m, color='blue', marker_kwds={'radius': 15})
+
+# The `ors_directions` function consumes a list of numeric vectors which contain the coordinates of origin and destination. You can also add up to 50 waypoints in between. Via the output parameter you can control the structure of the response. The parameters explained:
+#
+# -   profile: The default is `driving-car`, however `pedestrian`, different `cycle`, `heavy vehicles`, `wheelchair` are possible as well
+#
+# -   preference: The default is `fastest`. `shortest` and `recommended` are other possible values.
+#
+# -   options: Not set by default. It represents an additional object with further parameters to individualize the request like:
+#
+#     -   avoid_borders: Do not route through controlled borders, or none at all
+#
+#     -   avoid_countries: Do not route through specified countries.
+#
+#     -   avoid_features: Do not route via ferries or tolways
+#
+#     -   **avoid_polygons:** Do not route through a customized area. Must be formatted in GeoJSON as either a Polygon or Multipolygon object. Featurecollections are not supported
+#
+# Check out all available parameters with the interactive API playground here:
+#
+# [openrouteservice.org/dev/#/api-docs/v2/directions/{profile}/post](https://openrouteservice.org/dev/#/api-docs/v2/directions/%7Bprofile%7D/post)
+#
+# For the purpose of disaster aware routing we will now use the lastly mentioned parameter and define an area to be avoided by the route calculation.
 #
 
-# The basecamp where the rescue helpers were stationed is the Nürburgring
+# ## Disaster aware route from Nürburgring to Bad Neuanahr-Ahrweiler
+#
+# Now let's calculate the same route after the flood has destroyed roads and bridges. 
 
-basecamp_coordinates = (6.943488121032716, 50.33214859082482)
-basecamp = gpd.GeoDataFrame({'geometry': [Point(basecamp_coordinates)], 'name': ['Base Camp']}, crs="epsg:4326")
+
+
+
+
+# ### Calculate routes from base camp to the affected places 
+
+# Building upon the concepts and skills acquired, we tackle a more complex task. We utilize data from the Copernicus Emergency Mapping service, which provides information on destroyed or damaged roads and bridges in the Ahr valley, to calculate routes from the disaster relief staging area at the Nürburgring. Our objective is to reach 36 locations representing the affected communities in the valley.
+# For this purpose we need the following additional data:
+#
+# -   `affected_roads_bridges` All damaged and destroyed roads and bridges provided by the Copernicus Emergency Mapping Service. This dataset as well as others are available here: <https://emergency.copernicus.eu/mapping/list-of-components/EMSR517>
+#
+# -   `affected_places` All place=\* objects from OpenStreetMap that are located within the boundaries of municipalities that where affected.
+#
+# You find the files also in the repo this R-Markdown file is located in. <https://github.com/GIScience/openrouteservice-workshop-r/tree/master/data>
+
+# First, we need to add the affected roads and bridges to the quer parameters as areas to avoid.
 
 affected_roads_bridges = gpd.read_file("../resources/data/ahrtal/affected_roads.gpkg")
 affected_roads_bridges['id'] = affected_roads_bridges.index
 affected_places = gpd.read_file("../resources/data/ahrtal/affected_places.gpkg")
 
 
-# Buffer the damaged bridges and roads by 2 meters to convert them into polygons
+# As before we need to prepare the avoid areas for the openrouteservice query. The roads and bridges are of type `LINESTRING.` The APIs avoid area parameter however can only consume single polygons or multipolygons. Therefore we need convert it via buffer and union into one continuous multipolygon.
 
 affected_roads_bridges_union = affected_roads_bridges.copy()
 affected_roads_bridges_union.geometry = affected_roads_bridges_union.geometry.to_crs('25832').buffer(2).to_crs('4326').make_valid()
 affected_roads_bridges_union = affected_roads_bridges_union.dissolve()
 affected_roads_bridges_union.geometry = affected_roads_bridges_union.make_valid()
 
-m = affected_roads_bridges.explore(color='red',
-                                  tooltip=['id'])
-affected_places.explore(m=m, 
-                        color='blue')
+# Let's look at the data on a map. Affected places are shown in blue, damaged roads and bridges in red. 
 
-# ### Calculate routes from base camp to the affected places 
+m = affected_roads_bridges.explore(color='red', tooltip=['id'], tiles='cartodbpositron', legend=True)
+affected_places.explore(m=m, color='blue', legend=True)
 
-# First, we need to add the affected roads and bridges to the quer parameters as areas to avoid.
+#
+# Next we loop over all communities in `affected_places`. Some communities won't be reachable at all, they were cut off by the disaster. Openrouteservice will throw errors for these destinations, as it won't be able to create a route. We account for this circumstance with a `try except` block.
+
+# #### Define request parameters
+# The request parameters will be the same as before. But for the route request after flood, the roads to be avoided need to be addedn to the query.
 
 request_params_flood = request_params.copy()
 request_params_flood['options'] = {'avoid_polygons': mapping(affected_roads_bridges_union.make_valid().geometry[0])}
+
+# Now one request will be sent for each affected place. 
+#
+# **Note:** You might get warnings like `UserWarning: Rate limit exceeded. Retrying for the 3rd time.`. This means you have exceeded your minutly quote, just let the script run, it will retry sending the requests until they are successful. 
 
 # +
 normal_routes = []
@@ -129,6 +191,11 @@ for i, p in affected_places.iterrows():
 
 # -
 
+# We see, the communities of Altenburg, Brück 2, Schuld4, Pützfeld and Rech are unfortunately were no longer reachable via regular motorized transport. These communities were the first to be set up with temporary bridges.
+#
+# For the communities, however where could generate disaster aware routes, we are now able to determine access paths. Further we can determine the increase in travel time to the communities. For this purpose we need to do some postprocessing of our results. The chunk below will unpack served attributes by openrouteservice on time and distance information for every route. Then a time difference is calculated as new column.
+
+# +
 normal_routes_df = gpd.GeoDataFrame(pd.concat(normal_routes)).set_crs(epsg=4326).set_index('id')
 avoiding_routes_df = gpd.GeoDataFrame(pd.concat(avoiding_routes)).set_crs(epsg=4326).set_index('id')
 
@@ -138,6 +205,9 @@ avoiding_routes_df['duration'] = avoiding_routes_df.summary.map(lambda x: x['dur
 avoiding_routes_df = avoiding_routes_df.join(normal_routes_df[['duration']], lsuffix='_avoid')
 avoiding_routes_df['duration_difference'] = (avoiding_routes_df.duration_avoid - avoiding_routes_df.duration) / 60. 
 avoiding_routes_df['duration_difference_%'] = (avoiding_routes_df.duration_avoid - avoiding_routes_df.duration) / avoiding_routes_df.duration_avoid * 100. 
+# -
+
+
 
 fig, axes = plt.subplots(1,1, figsize=(15, 5))
 sns.barplot(data=avoiding_routes_df.sort_values('duration_difference_%', ascending=False), 
